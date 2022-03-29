de39968ddbf159256b5bab8a56dd5e8d6eada36e
kaichiehhsu <kaichieh@princeton.edu>
2022-03-29 18:38:43 -0400

    implements state2ell in ellReach

--- race_car/bicycle_ellipsoid.py
+++ race_car/bicycle_ellipsoid.py
@@ -17,21 +17,3 @@ class BicycleEllipsoid(BicycleDynamics):
     ego_q = np.array([self.wheelbase / 2, 0])[:, np.newaxis]
     ego_Q = np.diag([ego_a**2, ego_b**2])
     self.footprint = Ellipse(q=ego_q, Q=ego_Q)
-
-  def state2ell(self, state: np.ndarray) -> Union[Ellipse, List[Ellipse]]:
-    if state.ndim == 1:
-      theta = state[3]
-      d = state[:2][:, np.newaxis]
-      R = np.array([[np.cos(theta), -np.sin(theta)],
-                    [np.sin(theta), np.cos(theta)]])
-      return self.footprint @ R + d
-    else:
-      assert state.ndim == 2, "State shape is incorrect!"
-      ego_ell = []
-      for i in range(state.shape[1]):
-        theta = state[3, i]
-        d = state[:2, i][:, np.newaxis]
-        R = np.array([[np.cos(theta), -np.sin(theta)],
-                      [np.sin(theta), np.cos(theta)]])
-        ego_ell.append(self.footprint @ R + d)
-      return ego_ell
