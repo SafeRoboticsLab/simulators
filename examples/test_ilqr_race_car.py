8a723159ffe1238d52c227128d836dcec4f000f5
kaichiehhsu <kaichieh@princeton.edu>
2022-05-14 23:21:26 -0400

    adds save_obj and load_obj

--- examples/test_ilqr_race_car.py
+++ examples/test_ilqr_race_car.py
@@ -14,9 +14,8 @@ import argparse
 
 from simulators import (
     RaceCarSingleEnvV1, RaceCarSingleEnvV2, load_config, Ellipse,
-    plot_ellipsoids
+    plot_ellipsoids, save_obj
 )
-from utils import save_obj
 
 
 def main(config_file):
