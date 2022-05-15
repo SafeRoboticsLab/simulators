b89b703fbe78f44900a3ae996ac6a3840585e49d
kaichiehhsu <kaichieh@princeton.edu>
2022-05-14 23:14:32 -0400

    uses relative path and uses v2 yaml at default

--- examples/test_ilqr_race_car.py
+++ examples/test_ilqr_race_car.py
@@ -248,7 +248,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument(
       "-cf", "--config_file", help="config file path", type=str,
-      default=os.path.join("simulators", "race_car", "race_car_env_v1.yaml")
+      default=os.path.join("simulators", "race_car", "race_car_env_v2.yaml")
   )
   args = parser.parse_args()
   main(args.config_file)
