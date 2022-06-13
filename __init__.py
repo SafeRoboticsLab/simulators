491695b8c80866492ffdc119768fbd2ca3be6c0b
Nguyen Phuong Duy <duyn@princeton.edu>
2022-06-13 01:30:56 -0400

    add SpiritPybulletEnv to __init__ of simulators

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .config.utils import load_config
 
-# from .agent import Agent
+from .agent import Agent
 
 # from .race_car.track import Track
 # from .race_car.constraints_bicycle_v1 import ConstraintsBicycleV1
@@ -11,4 +11,4 @@ from .config.utils import load_config
 # from .ell_reach.ellipse import Ellipse
 # from .ell_reach.plot_ellipsoids import plot_ellipsoids
 
-from .dynamics.spirit_dynamics_pybullet import SpiritDynamicsPybullet
+from .spirit_rl.spirit_pybullet_env import SpiritPybulletEnv
