2de821458a967ab07cd4d90ede9469622789ed5a
Nguyen Phuong Duy <duyn@princeton.edu>
2022-06-17 16:39:56 -0400

    modification to work with isaacs and single agent

--- spirit_rl/spirit_pybullet_env.py
+++ spirit_rl/spirit_pybullet_env.py
@@ -1,88 +1,18 @@
+from abc import ABC
 from typing import Any, Tuple, Optional, Callable, List, Dict, Union
 import numpy as np
-from gym import spaces
+from gym import Space, spaces
 import torch
 from simulators.pybullet_debugger import pybulletDebug
-from ..base_single_env import BaseSingleEnv
 
-class SpiritPybulletEnv(BaseSingleEnv):
+class SpiritPybulletEnv(ABC):
+    """
+    Dummy environment when wanting to use Spirit with Pybullet
+    As the Spirit dynamics is stored in Pybullet and can be accessed through dynamics/spirit_dynamics_pybullet.py, this is just dummy env to pass data along
+
+    Args:
+        ABC (_type_): _description_
+    """
+
     def __init__(self, config_env: Any, config_agent: Any) -> None:
-        assert config_env.NUM_AGENTS == 1, "This environment only has one agent!"
-        super().__init__(config_env, config_agent)
-
-    def reset(self, state: Optional[np.ndarray] = None, cast_torch: bool = False, **kwargs) -> Union[np.ndarray, torch.FloatTensor]:
-        super().reset()
-        self.agent.dyn.reset()
-        obs = self.get_obs(None)
-
-        self.state = obs.copy()
-        
-        if cast_torch:
-            obs = torch.FloatTensor(obs)
-        return obs
-
-    def get_obs(self, state: np.ndarray) -> np.ndarray:
-        return self.agent.dyn.state
-    
-    def get_cost(self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray, constraints: Optional[Dict] = None) -> float:
-        # TODO: Write this function
-        # NOTE: in the old spirit_rl_env, costType is sparse and so the cost return is o
-        return 0
-
-    def get_constraints(self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray) -> Dict:
-        return self.agent.dyn.get_constraints()
-
-    def get_target_margin(self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray) -> Dict:
-        return self.agent.dyn.get_target_margin()
-
-    def get_done_and_info(self, constraints: Dict, targets: Dict, final_only: bool = True, end_criterion: Optional[str] = None) -> Tuple[bool, Dict]:
-        
-        if end_criterion is None:
-            end_criterion = self.end_criterion
-        
-        done = False
-        done_type = "not_raised"
-        if self.cnt >= self.timeout:
-            done = True
-            done_type = "timeout"
-        
-        g_x = max(list(constraints.values()))
-        l_x = max(list(targets.values()))
-        binary_cost = 1. if g_x > 0. else 0.
-
-        # Gets done flag
-        if end_criterion == 'failure':
-            failure = g_x > 0
-            if failure:
-                done = True
-                done_type = "failure"
-        elif end_criterion == 'reach-avoid':
-            failure = g_x > 0.
-            success = not failure and l_x <= 0.
-            
-            if success:
-                done = True
-                done_type = "success"
-            elif failure:
-                done = True
-                done_type = "failure"
-        elif end_criterion == 'timeout':
-            pass
-        else:
-            raise ValueError("End criterion not supported!")
-
-        # Gets info
-        info = {
-            "done_type": done_type,
-            "g_x": g_x,
-            "l_x": l_x,
-            "binary_cost": binary_cost
-        }
-
-        return done, info
-    
-    def render(self):
-        return super().render()
-    
-    def report(self):
-        return super().report()
+        pass
--- spirit_rl/spirit_pybullet_single.py
+++ spirit_rl/spirit_pybullet_single.py
@@ -1,31 +1,26 @@
-from ..base_zs_env import BaseZeroSumEnv
-from .spirit_pybullet_env import SpiritPybulletEnv
 from typing import Dict, Tuple, Any, Optional, Union
 import numpy as np
 import torch
 import matplotlib
-from ..utils import ActionZS
 
-class SpiritPybulletZeroSumEnv(BaseZeroSumEnv, SpiritPybulletEnv):
-    """
-    A wrapper for a zero-sum game env with a physical agent of using Spirit Pybullet dynamics
+from .spirit_pybullet_env import SpiritPybulletEnv
+from ..base_single_env import BaseSingleEnv
 
-    Args:
-        BaseZeroSumEnv
-        SpiritPybulletEnv
-    """
-
+class SpiritPybulletSingleEnv(BaseSingleEnv, SpiritPybulletEnv):
     def __init__(self, config_env: Any, config_agent: Any) -> None:
-        assert config_env.NUM_AGENTS == 2, "This is a zero-sum game!"
-        BaseZeroSumEnv.__init__(self, config_env, config_agent)
+        assert config_env.NUM_AGENTS == 1, "This environment only has one agent!"
+        BaseSingleEnv.__init__(self, config_env, config_agent)
         SpiritPybulletEnv.__init__(self, config_env, config_agent)
     
     def seed(self, seed: int = 0):
-        BaseZeroSumEnv.seed(self, seed)
+        BaseSingleEnv.seed(self, seed)
         SpiritPybulletEnv.seed(self, seed)
     
-    def reset(self, state: Optional[np.ndarray] = None, cast_torch: bool = False, **kwargs) -> Union[np.ndarray, torch.FloatTensor]:
-        BaseZeroSumEnv.reset(self, state, cast_torch, **kwargs)
+    def reset(
+        self, state: Optional[np.ndarray] = None, cast_torch: bool = False,
+        **kwargs
+    ) -> Union[np.ndarray, torch.FloatTensor]:
+        BaseSingleEnv.reset(self, state, cast_torch, **kwargs)
         self.agent.dyn.reset()
         obs = self.get_obs(None)
 
@@ -35,19 +30,27 @@ class SpiritPybulletZeroSumEnv(BaseZeroSumEnv, SpiritPybulletEnv):
             obs = torch.FloatTensor(obs)
         return obs
     
-    def get_cost(self, state: np.ndarray, action: ActionZS, state_nxt: np.ndarray, constraints: Optional[Dict] = None) -> float:
+    def get_cost(
+        self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray,
+        constraints: Optional[dict] = None
+    ) -> float:
         return 0
-    
-    def get_constraints(self, state: np.ndarray, action: ActionZS, state_nxt: np.ndarray) -> Dict:
+
+    def get_constraints(
+        self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray
+    ) -> Dict:
         return self.agent.dyn.get_constraints()
-    
-    def get_target_margin(self, state: np.ndarray, action: ActionZS, state_nxt: np.ndarray) -> Dict:
+
+    def get_target_margin(
+        self, state: np.ndarray, action: np.ndarray, state_nxt: np.ndarray
+    ) -> Dict:
         return self.agent.dyn.get_target_margin()
-    
+
     def get_done_and_info(
         self, constraints: Dict, targets: Optional[Dict] = None,
         final_only: bool = True, end_criterion: Optional[str] = None
     ) -> Tuple[bool, Dict]:
+        
         if end_criterion is None:
             end_criterion = self.end_criterion
         
@@ -94,10 +97,9 @@ class SpiritPybulletZeroSumEnv(BaseZeroSumEnv, SpiritPybulletEnv):
 
     def get_obs(self, state: np.ndarray) -> np.ndarray:
         return self.agent.dyn.state
-    
+
     def render(self):
         return super().render()
-    
+
     def report(self):
         return super().report()
-
