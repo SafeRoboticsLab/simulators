7ca6203f0d3e912def4e43c00a809549405df3a2
Kai-Chieh Hsu <62113661+kaichiehhsu@users.noreply.github.com>
2022-09-14 11:25:16 -0400

    Clean up race car environment with iLQR (#6)
    * minor
    
    * target set includes ref path matching; sim_traj returns length
    
    * path margin includes yaw
    
    * separates yaw and adds report
    
    * projects yaw to be with 2 pi
    
    * fixes yaw should be both clockwise and counter-clockwise; separates yaw and path
    
    * requires get_obs and implements for race car sim
    
    * amplifies success only; sets reset space seed
    
    * modularizes for the bicycle dynamics with delta vel ctrl
    
    * modularizes functions
    
    * implements bycycle dynamics with sterring velocity control
    
    * fix: get_obs had wrong dim
    
    * fix index error
    
    * wider track
    
    * modify reset sample space; use tan to get yaw margin
    
    * get obs for multiple states
    
    * add reach SDP
    
    * rm unfinished file
    
    * needs relative imporrt
    
    * uses relative path and uses v2 yaml at default
    
    * adds save_obj and load_obj
    
    * the safety module will be implemented under base_policy
    
    * update ell_reach (merge develop)
    
    * update ell_reach
    
    * update submodule
    
    * modifies adversary for bicycle v2
    
    * fixes config and ellReach
    
    * uses relative path
    
    Co-authored-by: Clack <huhm@shanghaitech.edu.cn>

--- __init__.py
+++ __init__.py
@@ -10,3 +10,5 @@ from .race_car.race_car_single_v2 import RaceCarSingleEnvV2
 
 from .ell_reach.ellipse import Ellipse
 from .ell_reach.plot_ellipsoids import plot_ellipsoids
+
+from .utils import save_obj
